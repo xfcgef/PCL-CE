79d16f2358ab35c0615e13943fc73eae8e25516c
Hill23333 <104297546+Hill23333@users.noreply.github.com>
2026-06-05 23:25:50 +0800

    feat: 重新实现 Toast 通知（第一部分：基础通知） (#2983)
    * feat: 两个vtbl
    
    * feat: 一些包装
    
    * chore: Delete -> DeleteHString
    
    * chore: 更改作用域
    
    * feat: 提供基本的 RoApi 封装
    
    * feat: 添加info方便调用
    
    * chore: 更改注释
    
    * fix: typo
    
    * chore: 更改注释
    
    * feat: IToastNotificationManagerStatics
    
    * feat: IToastNotifier
    
    * fix: typo (IToastNotifierStaticsInfo -> IToastNotifierInfo)
    
    * feat: 可用的 Toast
    
    * fix: 注册 aumid
    
    * chore: unittest 注释
    
    * chore: 注释
    
    * chore: aumid 注释
    
    * feat: UninstallToasts
    
    * chore: 再加一个注释
    
    * fix: 未释放资源
    
    * fix: 未 dispose
    
    * fix: 从 HSTRING 获取 string 时未判断空
    
    * chore: 统一实现
    
    * chore: 添加注释
    
    * fix
    
    * chore: 添加常量

--- PCL.Core.Test/ToastTest.cs
+++ PCL.Core.Test/ToastTest.cs
@@ -10,6 +10,7 @@ public class ToastTest
     [TestMethod]
     public void TestToast()
     {
+        // 别跑会炸，因为 Basics.cs 的 Metadata 需要在运行时加载，单元测试项目无法访问
         SendToast("A toast notice from PCL.Core!", "Test Toast");
     }
 }
